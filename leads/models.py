8d096c737f2b2f7a2ea3502fd8815a47db986307
z3rofactorial <shaurya.siffy@gmail.com>
2023-08-12 09:54:03 +0530

    updates in leads

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
 class Lead(models.Model):
     fname=models.CharField(max_length=10)
     lname=models.CharField(max_length=10)
     age=models.IntegerField(default=0)
-    
+
+    lead_agent=models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
